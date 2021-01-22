8d8f807c81e50a5b963b1853d50dece6c832423a
JuliaKletsova <thugkek@DESKTOP-8O7BRC5.localdomain>
2021-01-22 22:56:31 +0300

    test

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-data-table
+    :headers="headers"
+    :item="data"
+    :items-per-page="10"
+    class="elevation-1"
+  ></v-data-table>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+      headers: [
+            { text: 'hints', value: 'hints' },
+            { text: 'name', value: 'name' },
+            { text: 'type', value: 'type' }
+      ],
+      data: []      
+  }),
+  mounted() {
+    this.$axios.get("https://data.jsdelivr.com/v1/stats/packages").then(response => {
+        for(let i=0;i<response.data.length;i++) {
+          this.data.push(response.data[i])
+        console.log(this.data.length)
+        }
+        console.log(this.data)
+    })
+  }
+}
+</script>
